2cb56b0c7f3f92dc0724740a40a608d1f5f7a407
Mohamed-Amin <166614121+MoohamedAmin-wo@users.noreply.github.com>
2024-10-26 13:10:05 +0300

    Complete Categories Controller

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -58,5 +58,52 @@ namespace Shopify.API.Controllers
 
 			return Ok(_mapper.Map<CategoryDetailsDTO>(category));
 		}
+
+		[HttpDelete("DeleteAsync")]
+		public async Task<IActionResult> Delete(int id)
+		{
+			var category = await _unitOfWork.categories.FindByIdAsync(id);
+			if (category is null)
+				return NotFound($"Couldn't find Category with Id {id}!");
+
+			await _unitOfWork.categories.DeleteAsync(category);
+			_unitOfWork.Commit();
+
+			return Ok($"{_mapper.Map<CategoryDetailsDTO>(category).Name} has been Deleted");
+		}
+
+		[HttpGet("FindByIdAsync")]
+		public async Task<IActionResult> FindById(int id)
+		{
+			var category = await _unitOfWork.categories.FindByIdAsync(id);
+			if (category is null)
+				return NotFound($"Couldn't find Category with Id {id}!");
+
+			if (category.IsDeleted == true)
+				return BadRequest("This Category is disActive any more !");
+
+			return Ok(_mapper.Map<CategoryDetailsDTO>(category));
+		}
+		[HttpGet("FindByName")]
+		public async Task<IActionResult> FindByName(string name)
+		{
+			var category = await _unitOfWork.categories.FindByCreatriaAsync(c=>c.Name.Contains(name));
+			if (category is null)
+				return NotFound($"Couldn't find Category with this name {name}");
+
+			return Ok(_mapper.Map<CategoryDetailsDTO>(category));
+		}
+		[HttpGet("GetAllCategories")]
+		public async Task<IActionResult> GetAll()
+		{
+			var categoreis = await _unitOfWork.categories.FindByCreatriaAsync(
+				predicate : x=>x.IsDeleted == false,
+				stopTracking : true);
+
+			if (categoreis is null)
+				return BadRequest("no Categories were added yet !");
+
+			return Ok(_mapper.Map<IReadOnlyCollection<CategoryDetailsDTO>>(categoreis));
+		}
 	}
 }
