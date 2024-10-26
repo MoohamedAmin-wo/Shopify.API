12e2da6a81ae89cb1a1053c6d9ec80525c927f6f
Mohamed-Amin <166614121+MoohamedAmin-wo@users.noreply.github.com>
2024-10-26 11:46:48 +0300

    Create The Configurations for Entities and Create The Database

---
+++ Data/Configurations/CategoryConfigurations.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Shopify.API.Models;
+
+namespace Shopify.API.Data.Configurations
+{
+	public class CategoryConfigurations : IEntityTypeConfiguration<Category>
+	{
+		public void Configure(EntityTypeBuilder<Category> builder)
+		{
+			builder.ToTable("Categories" , "A001");
+			builder.Property(p => p.Name).HasMaxLength(200).IsRequired() ;
+			builder.Property(p => p.Description).HasMaxLength(700).IsRequired();
+			builder.Property(p => p.IsDeleted).HasDefaultValue(false);
+		}
+	}
+}
--- obj/Debug/net8.0/Shopify.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Shopify.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shopify.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6febdd257f0ebf54c23459b63ccb64d67078f39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a92f452aab38dc156b6ac714167d8450a6a0bedb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shopify.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shopify.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
